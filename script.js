7645b8dba7593bf2d8637e843a8c4e67a928320c
Atakan Reyhanioglu <atakan.reyhanioglu@gmail.com>
2023-06-06 09:51:38 +0300

    portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('twitter-btn');
+
+// Add the event listener to the button
+button.addEventListener('click', function() {
+    window.open('https://twitter.com/atakanRG')
+});
